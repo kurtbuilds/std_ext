e62cfc177f046eb00840c6531b73c4173cb56d8d
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-11-01 12:59:01 -0700

    add unwrapinfallible

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,16 @@ mod map;
 mod option;
 mod path;
 pub mod path2;
+mod result;
 mod str;
-mod vec_ext;
+mod vec;
 
 pub use command::*;
 pub use option::*;
 pub use path::*;
+pub use result::*;
 pub use str::*;
-pub use vec_ext::*;
+pub use vec::*;
 
 pub fn default<T: Default>() -> T {
     T::default()
