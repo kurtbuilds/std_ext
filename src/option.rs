4ee161875abca655fea90d6599d98d863ff6e594
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-12-21 15:27:03 -0500

    add OptionExt

--- src/option.rs
+++ src/option.rs
@@ -3,3 +3,13 @@ pub use string::*;
 
 mod vec;
 pub use vec::*;
+
+pub trait OptionExt<T> {
+    fn map_ref<U>(&self, f: impl FnOnce(&T)-> U) -> Option<U>;
+}
+
+impl<T> OptionExt<T> for Option<T> {
+    fn map_ref<U>(&self, f: impl FnOnce(&T)-> U) -> Option<U> {
+        self.as_ref().map(f)
+    }
+}
